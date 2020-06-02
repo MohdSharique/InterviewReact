931fe37c3456f5bf0c166a548110e47918a1554d
ShariqueMohd <1sharique1@gmail.com>
2020-06-02 19:07:37 +0530

    Added get/post/patch APIs support

--- app/javascript/components/Participants.jsx
+++ app/javascript/components/Participants.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {NavLink} from 'react-router-dom';
+import axios from 'axios';
 const Participants = () => {
+
+    const [participants, setParticipants] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/participants').then((res) => {
+            console.log(res.data);
+            setParticipants(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
+
     return (
-        <div id = "interviews">
-            <h1> Interview List </h1>
+        <div id = "participants">
+            <h1> Participants List </h1>
             <table>
             <thead>
             <tr>
@@ -13,13 +27,16 @@ const Participants = () => {
             </thead>
         
             <tbody>
-                <tr>
-                    <td> asd@gmail.com </td>
-                    <td> Interviewer </td>
-                    <td><NavLink exact to="/participants/:id/show">Show</NavLink></td>
-                    <td><NavLink exact to="/participants/:id/edit">Edit</NavLink></td>
-                    <td><NavLink exact to="/participants/:id/destroy">Destroy</NavLink></td>
-                </tr>
+            {
+                    participants.map(participant => (
+                        <tr key={participant.id}>
+                            <td> {participant.email} </td>
+                            <td> {participant.role} </td>
+                            <td><NavLink exact to="/participant/:id/edit">Edit</NavLink></td>
+                            <td><NavLink exact to="/participant/:id/destroy">Destroy</NavLink></td>
+                        </tr>
+                    ))
+                }
             </tbody>
         </table>
       </div>
--- app/javascript/components/ShowInterview.jsx
+++ app/javascript/components/ShowInterview.jsx
@@ -1,8 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {NavLink} from 'react-router-dom';
-const Main = () => {
+import axios from 'axios';
+const Main = (props) => {
+    console.log(props)
+
+    var id = props.match.params.id;
+    // const [detail, setDetail] = useState([]);
+    
+    const [interview, setInterview] = useState({});
+    const [participants, setParticipants] = useState([]);
+
+    const [email, setEmail] = useState("");
+    const [role, setRole] = useState("");
+    
+    useEffect(() => {
+        axios.get(`http://localhost:3000/interviews/${id}`).then((res) => {
+            console.log(res.data);
+            setInterview(res.data.interview)
+            setParticipants(res.data.participants)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
+
+    const participantSubmit = (e) =>{
+        e.preventDefault();
+        let data= {
+          participant: {
+            email: email,
+            role: role,
+            interview_id: props.match.params.id
+          }
+        }
+    
+        axios.post(`http://localhost:3000/interviews`, {data})
+          .then(res => {
+            console.log(res);
+          })
+      };
+
+    // console.log(interview.title)
+    // console.log(detail.interview['title'])
+    // console.log(detail.interview.title)
     return (
-        <div> Show Interview </div>
+        <div> 
+            <h1> Show Interview </h1> 
+            {!interview ? ("") : (
+                <div>
+                    <strong> Title: </strong> {interview.title} <br/>
+                    <strong> Start Time: </strong> {interview.start_time} <br/>
+                    <strong> End Time:</strong> {interview.end_time} <br/>
+                </div>
+            )}
+            <br/>
+            {!participants ? ("") : (
+            <div id = "participants">    
+                <h3> Participants</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Email</th>
+                        <th>Role</th>
+                    </tr>
+                    </thead>
+                
+                    <tbody>
+                        {
+                            participants.map(participant => (
+                                <tr key={participant.id}>
+                                    <td> {participant.email} </td>
+                                    <td> {participant.role} </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>    
+            </div>     
+            )}
+            <br/><h3> Add Participant</h3>
+            <form onSubmit = {participantSubmit}>
+                <label>
+                    Email :
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </label><br></br>
+                <label>
+                    Role :
+                    <input type="datetime-local" value={role} onChange={(e) => setRole(e.target.value)}/>
+                </label><br></br>
+                <input type="submit" value="Submit" />
+            </form>
+            
+        </div>
     );
 }
 
